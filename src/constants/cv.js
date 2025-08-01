50ee9095289d4eda8bcc05ff62cb18a09a5f011a
Delpo <guido_delponte@hotmail.com>
2025-08-01 16:01:09 -0300

    cvData-Header -> URL Photo Fix

--- src/constants/cv.js
+++ src/constants/cv.js
@@ -14,7 +14,7 @@ export const cvData = {
       name: "Guido Delponte",
       title: "Python Developer & Systems Analyst",
       description: "𝐏𝐲𝐭𝐡𝐨𝐧 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 y 𝐀𝐧𝐚𝐥𝐢𝐬𝐭𝐚 𝐝𝐞 𝐒𝐢𝐬𝐭𝐞𝐦𝐚𝐬 con más de 3 años de experiencia en 𝐝𝐞𝐬𝐚𝐫𝐫𝐨𝐥𝐥𝐨 𝐝𝐞 𝐬𝐨𝐟𝐭𝐰𝐚𝐫𝐞 y 𝐚𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐳𝐚𝐜𝐢𝐨𝐧𝐞𝐬. Especializado en crear soluciones eficientes utilizando 𝐏𝐲𝐭𝐡𝐨𝐧, desde automatizaciones y procesamiento de datos hasta desarrollo de 𝐀𝐏𝐈𝐬 y 𝐚𝐩𝐥𝐢𝐜𝐚𝐜𝐢𝐨𝐧𝐞𝐬 𝐰𝐞𝐛.<br> Combino sólidos 𝐜𝐨𝐧𝐨𝐜𝐢𝐦𝐢𝐞𝐧𝐭𝐨𝐬 𝐭é𝐜𝐧𝐢𝐜𝐨𝐬 con excelentes 𝐡𝐚𝐛𝐢𝐥𝐢𝐝𝐚𝐝𝐞𝐬 𝐝𝐞 𝐜𝐨𝐦𝐮𝐧𝐢𝐜𝐚𝐜𝐢ó𝐧 y relacionamiento interpersonal, lo que me permite colaborar efectivamente tanto con equipos técnicos como con clientes y stakeholders. De perfil 𝐩𝐫𝐨𝐚𝐜𝐭𝐢𝐯𝐨 y 𝐨𝐫𝐢𝐞𝐧𝐭𝐚𝐝𝐨 𝐚 𝐫𝐞𝐬𝐮𝐥𝐭𝐚𝐝𝐨𝐬, disfruto resolver problemas complejos y aportar mejoras continuas a los sistemas.",
-      photo: "/src/assets/images/profile_picture.jpg"
+      photo: "https://avatars.githubusercontent.com/u/20926742?s=400&u=1d6b6e72a58909e3767e8334a0d889d052eeaf0d&v=4"
     },
     contact: {
       email: "guido_delponte@hotmail.com",
